08ab4b9f3e7f6889f23fc160d57b0eae62ee5a7f
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-05-23 15:55:35 +0530

    axios interceptor added

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,12 +1,31 @@
 import axios from "axios";
+import { signOut } from "@/redux/slices/user";
+import { useDispatch } from "react-redux";
 
 const useAxios = () => {
-  return axios.create({
+  const dispatch = useDispatch();
+
+  const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_API_BASE_URL,
     headers: {
       "Content-Type": "application/json",
     },
   });
+
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (err) => {
+      if (err.response && err.response.status === 401) {
+        dispatch(signOut());
+      }
+
+      return Promise.reject(err);
+    }
+  );
+
+  return axiosInstance;
 };
 
 export default useAxios;

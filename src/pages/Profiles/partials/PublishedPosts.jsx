62f3620bf2a1024ec79be0b8691f0f0b7b28df6f
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-06-11 15:49:50 +0530

    post comm and like listner added in single profile

--- src/pages/Profiles/partials/PublishedPosts.jsx
+++ src/pages/Profiles/partials/PublishedPosts.jsx
@@ -4,11 +4,16 @@ import { useEffect } from "react";
 import useAllPosts from "@/api/posts/All";
 import PostCard from "@/components/Posts/Card";
 import PostCardSkeleton from "@/components/Posts/CardSkeleton";
+import usePostCommentListner from "@/listners/post/Comment";
+import usePostLikeListner from "@/listners/post/Like";
+import { useSelector } from "react-redux";
 
 const PublishedPosts = ({ userId, setSelectedTab }) => {
+  const authUser = useSelector((state) => state.authUser);
   const {
     allPostsReq,
     data: dataAllPosts,
+    setData: setDataAllPosts,
     isLoading: isLoadingAllPosts,
     reFetchAllPosts,
     error: errorAllPosts,
@@ -22,6 +27,97 @@ const PublishedPosts = ({ userId, setSelectedTab }) => {
   useEffect(() => {
     allPostsReq(`?per_page=10&user_id=${userId}`);
   }, [userId]);
+
+  const postCommentListnerHandler = (e) => {
+    console.log("postCommentListnerHandler: ", e);
+
+    if (e.type === "created") {
+      setDataAllPosts((prev) => {
+        const updatedData = prev.data.map((post) => {
+          if (post.id === e.comment.post_id) {
+            return {
+              ...post,
+              comments: [e.comment, ...post.comments],
+            };
+          }
+          return post;
+        });
+
+        return {
+          ...prev,
+          data: updatedData,
+        };
+      });
+    }
+
+    if (e.type === "deleted") {
+      setDataAllPosts((prev) => {
+        const updatedData = prev.data.map((post) => {
+          if (post.id === e.comment.post_id) {
+            return {
+              ...post,
+              comments: post.comments.filter(
+                (comment) => comment.id !== e.comment.id
+              ),
+            };
+          }
+          return post;
+        });
+
+        return {
+          ...prev,
+          data: updatedData,
+        };
+      });
+    }
+
+    if (e.type === "updated") {
+      setDataAllPosts((prev) => {
+        const updatedData = prev.data.map((post) => {
+          if (post.id === e.comment.post_id) {
+            return {
+              ...post,
+              comments: post.comments.map((comment) => {
+                if (comment.id === e.comment.id) {
+                  return e.comment;
+                }
+                return comment;
+              }),
+            };
+          }
+          return post;
+        });
+        return {
+          ...prev,
+          data: updatedData,
+        };
+      });
+    }
+  };
+  usePostCommentListner(postCommentListnerHandler);
+
+  const postLikeListnerHandler = (e) => {
+    // console.log("postLikeListnerHandler: ", e);
+
+    setDataAllPosts((prev) => {
+      const updatedData = prev.data.map((post) => {
+        if (post.id === e.post.id) {
+          return {
+            ...post,
+            likes: e.post.likes,
+            is_liked: e.post.likes.some((like) => like.user_id === authUser.id),
+          };
+        }
+        return post;
+      });
+
+      return {
+        ...prev,
+        data: updatedData,
+      };
+    });
+  };
+  usePostLikeListner(postLikeListnerHandler);
 
   return (
     <>
